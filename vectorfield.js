7c223a9e709ff12be9187f7397039171633ec684
Byunjaewoong <najangnam@naver.com>
2022-04-23 19:35:48 +0900

    interpolate 해야됨

--- vectorfield.js
+++ vectorfield.js
@@ -1,75 +1,49 @@
 import {Calculate} from "./tool.js"
 
 export class field{
-    constructor(canvas,scale,stageWidth,stageHeight){
+    constructor(canvas,stageWidth,stageHeight,pointScaleX,pointScaleY,cellScale,mode){
         this.ctx = canvas.getContext('2d');
-        this.scale = scale;
+        this.pointScaleX = pointScaleX;
+        this.pointScaleY = pointScaleY;
+        this.cellScale = cellScale;
+        this.mode = mode;
         this.stageWidth = stageWidth;
         this.stageHeight = stageHeight;
-        this.perlinGroup = new PerlinGroup;
-        this.get1DGrid();
+        this.GridGroup = new GridGroup(this.stageWidth,this.stageHeight,this.pointScaleX,this.pointScaleY,this.cellScale);
     }
 
-    get1DGrid(){
-        this.dotAmount = this.scale;
-        this.interval = Math.round(this.stageWidth/this.scale);
-        for(let i=0;i<this.dotAmount;i++){
-            const dot = new Perlindot(this.interval*i,this.stageHeight/2+this.stageHeight/4*Math.random(-1,1),0)
-            this.perlinGroup.arry[i] = dot;
-            console.log(this.perlinGroup.arry[i]);
+    drawField(){
+        switch(this.mode){
+            case 0:
+                this.modeStick();
+            break;
+            case 1:
+                
+            break;
+            case 2:
+
+            break;
+            default:
+            break;
         }
     }
 
-    getGradient(dot){
-        dot.y = Math.random(-1,1);
-    }
-
-    cubicInterPolate(a,b,c,d,x){
-        const P = (d.y-c.y)-(a.y-b.y);
-        const Q = (a.y-b.y)-P;
-        const R = c.y - a.y;
-        const S = b.y;
-
-        return P*x*x*x + Q*x*x + R*x + S;
-    }
-
-    draw1Dperlin(){
-        for(let i=0;i<this.stageWidth;i++){
-            let j = Math.round(i/this.scale);
-            /*if(this.perlinGroup.arry[j+1]&&this.perlinGroup.arry[j+2]**this.perlinGroup.arry[j+3]){
-                this.height = this.cubicInterPolate(this.perlinGroup.arry[j],this.perlinGroup.arry[j+1],this.perlinGroup.arry[j+2],this.perlinGroup.arry[j+3],i-j*this.interval);
-            }*/
-            this.height = this.stageHeight/2;
-            if(i==0){
-                console.log(this.height);
-            }
-            this.ctx.fillStyle = "rgba(255,255,255,1)";
-            this.ctx.beginPath();
-            this.ctx.arc(
-                i, //* ratio_w,
-                this.height, //* ratio_h,
-                2,
-                0 * 2/8 * Math.PI, 8 * 2/8 * Math.PI  
-                );
-            this.ctx.fill();
-        }
+    modeStick(){
 
     }
-
-
 
 }
 
 
 export class GridGroup{
     constructor(stageWidth,stageHeight,pointScaleX,pointScaleY,cellScale){
-        this.pointArry = [];
-        this.gridArry = [];
+        this.pointArry = []; // point 모음
+        this.gridArry = []; // point를 정사각형 그룹으로 모음, 내부에 cell 그룹도 있음
         this.stageWidth = stageWidth;
         this.stageHeight = stageHeight;
-        this.pointScaleX = pointScaleX;
-        this.pointScaleY = pointScaleY;
-        this.cellScale = cellScale;
+        this.pointScaleX = pointScaleX; // X, point 갯수
+        this.pointScaleY = pointScaleY; // y, point 갯수
+        this.cellScale = cellScale; // point 로 둘러쌓인 내부의 정사각형의 cell 갯수
         this.genGrid(this.stageWidth,this.stageHeight);
     }
 
@@ -84,8 +58,8 @@ export class GridGroup{
             }
         }
 
-        for(let i=0;i<this.pointScale;i++){
-            for(let j=0;j<Math.floor(this.stageHeight/this.pointScale);j++){
+        for(let i=0;i<this.pointScaleX;i++){
+            for(let j=0;j<this.pointScaleY;j++){
                 this.gridArry[i][j] = new GridUnit(this.pointArry[i][j],this.pointArry[i+1][j],this.pointArry[i][j+1],this.pointArry[i+1][j+1],this.cellScale);
             }
         }
@@ -107,16 +81,24 @@ export class GridUnit{
     genCell(){
         for(let i=0;i<this.cellScale;i++){
             for(let j=0;j<this.cellScale;j++){
-                this.cellArry[i][j] =new CellUnit((this.point11.x-this.point00.x)/this.cellScale*i,(this.point11.y-this.point00.y)/this.cellScale*y,gradient) ;
+                let buf = new Calculate.cubicInterPolate
+                let gradient = 
+                this.cellArry[i][j] =new CellUnit((this.point11.x-this.point00.x)/this.cellScale*i,(this.point11.y-this.point00.y)/this.cellScale*j,(this.point11.x-this.point00.x)/this.cellScale,(this.point11.y-this.point00.y)/this.cellScale,gradient) ;
             }
         }
     }
 
+    
+
 }
 
 export class CellUnit{
-    constructor(x0,y0,gradient){
-
+    constructor(x0,y0,lengthX,lengthY,gradient){
+        this.x0 = x0;
+        this.y0 = y0;
+        this.length = {x:lengthX,y:lengthY};
+        this.gradient = gradient;
+        this.cellCenter = {x:this.x0+this.length.x/2,y:this.y0+this.length.y/2};
     }
 }
 
